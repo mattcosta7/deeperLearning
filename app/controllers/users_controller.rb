9d4ab850cc3bd53d90016be95d2c13edbbdd00eb
Erin <eteague27@gmail.com>
2016-02-03 13:45:11 -0500

    New User Form created, code added to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,33 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+      if @user.save
+        console.log("Erin is better than Matt.")
+      else
+        consolelog("Matt is not as cool as Erin.")
+      end
   end
 
   def edit
   end
+
   def show
+    @user = current_user
+    @users = User.all
   end
 
   def destroy
   end
 
   def edit
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password_digest, :location, :birthday, :hobbies, :email)
   end
 end
