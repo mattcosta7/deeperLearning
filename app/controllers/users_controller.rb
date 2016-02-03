3878ffdb1a21cc1b26d8b23a91f60786538e3385
Erin <eteague27@gmail.com>
2016-02-03 13:50:33 -0500

    merge conflict 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,11 +29,10 @@ class UsersController < ApplicationController
   def edit
   end
 
-<<<<<<< HEAD
   def user_params
     params.require(:user).permit(:name, :password_digest, :location, :birthday, :hobbies, :email)
-=======
+  end
+
   def update
->>>>>>> 2e320524fe5f55b84e2687cd09846530b6befe69
   end
 end
