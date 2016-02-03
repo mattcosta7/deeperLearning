b817bd1c7f4bd39b6c89fc6cfb083a6f2f118fca
Matthew Costabile <mattcosta7@gmail.com>
2016-02-03 13:38:08 -0500

    added courses controller removed some extra views

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,22 @@
+class CoursesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+end
