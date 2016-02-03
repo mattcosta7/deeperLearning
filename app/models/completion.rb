f248a6252958ee794f12561c2316b1d18341c4a2
Matthew Costabile <mattcosta7@gmail.com>
2016-02-03 16:03:42 -0500

    Added completions and completeds to users and challenges

---
+++ app/models/completion.rb
@@ -0,0 +1,8 @@
+class Completion < ActiveRecord::Base
+
+    belongs_to :completed, class_name: "Challenge"
+    belongs_to :completer, class_name: 'User'
+
+    validates :completed_id, uniqueness: {scope: :completer_id}
+    validates :completer_id, uniqueness: {scope: :completed_id}
+end
