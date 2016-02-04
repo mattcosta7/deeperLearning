29f234616cea7ab24d4c611c5c5439f55e9e956d
Erin <eteague27@gmail.com>
2016-02-04 11:19:08 -0500

    Overwrote bootstrap css to fix background bug

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20160203205354) do
   end
 
   create_table "courses", force: :cascade do |t|
-    t.string   "name"
+    t.string   "names"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
