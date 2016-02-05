3ab3a4959f3a4a8b410a2851a981125ed725fa2b
Matthew Costabile <mattcosta7@gmail.com>
2016-02-05 15:35:01 -0500

    ace fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require ace/theme-tomorrow_night_blue
+//= require ace/mode-javascript
 //= require ace-rails-ap
 //= require jquery
 //= require jquery_ujs
